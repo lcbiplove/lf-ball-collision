b51ebb3a726eaf4e8948d82afce4d8991dc905fa
lcbiplove <lcbiplove1@gmail.com>
2021-05-11 22:02:06 +0545

    Version 1 minor fix

--- js/BallCollision.js
+++ js/BallCollision.js
@@ -6,6 +6,7 @@ function BallCollision(props) {
 
   this.numOfBalls = props.numOfBalls || DEFAULT_NUM_OF_BALLS;
   this.colors = props.colors || DEFAULT_COLORS;
+  console.log(this.colors);
   this.balls = [];
   this.ballSizes = props.ballSizes || DEFAULT_BALL_SIZES;
 
@@ -18,7 +19,7 @@ function BallCollision(props) {
     const x = Math.random() * this.width;
     const y = Math.random() * this.height;
 
-    const color = DEFAULT_COLORS[ind % DEFAULT_COLORS.length];
+    const color = this.colors[ind % this.colors.length];
     const radius = this.ballSizes[ind % this.ballSizes.length];
 
     radius / balls.push(new Ball(x, y, radius, color, this.context));
